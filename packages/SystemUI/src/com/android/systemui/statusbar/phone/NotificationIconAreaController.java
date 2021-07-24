Spark-Rom/frameworks_base

Make colored Statusbar Icons optional [1/2]

packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationIconAreaController.java
@@ -1,5 +1,7 @@
 package com.android.systemui.statusbar.phone;
 
+import android.os.UserHandle;
+import android.provider.Settings;
 import android.app.NotificationManager;
 import android.content.Context;
 import android.content.res.Resources;
@@ -76,6 +78,7 @@ public class NotificationIconAreaController implements DarkReceiver,
     private Context mContext;
     private int mAodIconAppearTranslation;
 
+    boolean NewIconStyle;
     private boolean mAnimationsEnabled;
     private int mAodIconTint;
     private boolean mFullyHidden;
@@ -120,6 +123,10 @@ public class NotificationIconAreaController implements DarkReceiver,
 
         initializeNotificationAreaViews(context);
         reloadAodColor();
+
+    NewIconStyle = Settings.System.getIntForUser(mContext.getContentResolver(),
+            Settings.System.STATUSBAR_ICONS_STYLE, 1, UserHandle.USER_CURRENT) == 1;
+
     }
 
     protected View inflateIconArea(LayoutInflater inflater) {
@@ -516,7 +523,7 @@ public class NotificationIconAreaController implements DarkReceiver,
         if (colorize) {
             color = DarkIconDispatcher.getTint(mTintArea, v, tint);
         }
-        if (v.getStatusBarIcon().pkg.contains("systemui")) {
+        if (v.getStatusBarIcon().pkg.contains("systemui") || !NewIconStyle) {
             v.setStaticDrawableColor(color);
             v.setDecorColor(tint);
         } else {

packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationIconContainer.java
@@ -19,6 +19,9 @@ package com.android.systemui.statusbar.phone;
 import static com.android.systemui.statusbar.phone.HeadsUpAppearanceController.CONTENT_FADE_DELAY;
 import static com.android.systemui.statusbar.phone.HeadsUpAppearanceController.CONTENT_FADE_DURATION;
 
+import android.os.UserHandle;
+import android.provider.Settings;
+import android.content.ContentResolver;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.content.Context;
@@ -171,6 +174,9 @@ public class NotificationIconContainer extends AlphaOptimizedFrameLayout {
     private Rect mIsolatedIconLocation;
     private int[] mAbsolutePosition = new int[2];
     private View mIsolatedIconForAnimation;
+
+        boolean NewIconStyle = Settings.System.getIntForUser(getContext().getContentResolver(),
+                Settings.System.STATUSBAR_ICONS_STYLE, 1, UserHandle.USER_CURRENT) == 1;
 
     public NotificationIconContainer(Context context, AttributeSet attrs) {
         super(context, attrs);
@@ -803,7 +809,7 @@ public class NotificationIconContainer extends AlphaOptimizedFrameLayout {
                     }
                 }
                 icon.setVisibleState(visibleState, animationsAllowed);
-                if (icon.getStatusBarIcon().pkg.contains("systemui"))
+                if (icon.getStatusBarIcon().pkg.contains("systemui") || !NewIconStyle)
                     icon.setIconColor(iconColor, needsCannedAnimation && animationsAllowed);
                 if (animate) {
                     animateTo(icon, animationProperties);
@@ -827,7 +833,7 @@ public class NotificationIconContainer extends AlphaOptimizedFrameLayout {
             super.initFrom(view);
             if (view instanceof StatusBarIconView) {
                 StatusBarIconView icon = (StatusBarIconView) view;
-                if (icon.getStatusBarIcon().pkg.contains("systemui"))
+                if (icon.getStatusBarIcon().pkg.contains("systemui") || !NewIconStyle)
                     iconColor = ((StatusBarIconView) view).getStaticDrawableColor();
                 else
                     return;

CAF-MOD/frameworks_base-1

Change-Id: I66dc648fe66f492452b3c06acdde379e7a1c07dd

packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationIconAreaController.java
@@ -1,5 +1,7 @@
 package com.android.systemui.statusbar.phone;
 
+import android.os.UserHandle;
+import android.provider.Settings;
 import android.app.NotificationManager;
 import android.content.Context;
 import android.content.res.Resources;
@@ -77,6 +79,7 @@ public class NotificationIconAreaController implements DarkReceiver,
     private Context mContext;
     private int mAodIconAppearTranslation;
 
+    boolean NewIconStyle;
     private boolean mAnimationsEnabled;
     private int mAodIconTint;
     private boolean mFullyHidden;
@@ -124,8 +127,9 @@ public class NotificationIconAreaController implements DarkReceiver,
         initializeNotificationAreaViews(context);
         reloadAodColor();
 
-        mColorizedIcons = Settings.Global.getInt(mContext.getContentResolver(),
+        NewIconStyle = Settings.Global.getInt(mContext.getContentResolver(),
             Settings.Global.BAIKALOS_STATUSBAR_COLOR_ICONS, 0) == 1;
+
     }
 
     protected View inflateIconArea(LayoutInflater inflater) {
@@ -522,7 +526,7 @@ public class NotificationIconAreaController implements DarkReceiver,
         if (colorize) {
             color = DarkIconDispatcher.getTint(mTintArea, v, tint);
         }
-        if (v.getStatusBarIcon().pkg.contains("systemui")) {
+        if (v.getStatusBarIcon().pkg.contains("systemui") || !NewIconStyle) {
             v.setStaticDrawableColor(color);
             v.setDecorColor(tint);
         } else {

packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationIconContainer.java
@@ -19,6 +19,9 @@ package com.android.systemui.statusbar.phone;
 import static com.android.systemui.statusbar.phone.HeadsUpAppearanceController.CONTENT_FADE_DELAY;
 import static com.android.systemui.statusbar.phone.HeadsUpAppearanceController.CONTENT_FADE_DURATION;
 
+import android.os.UserHandle;
+import android.provider.Settings;
+import android.content.ContentResolver;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.content.Context;
@@ -173,9 +176,8 @@ public class NotificationIconContainer extends AlphaOptimizedFrameLayout {
     private int[] mAbsolutePosition = new int[2];
     private View mIsolatedIconForAnimation;
 
-    private boolean mColorizedIcons = Settings.Global.getInt(mContext.getContentResolver(),
-            Settings.Global.BAIKALOS_STATUSBAR_COLOR_ICONS, 0) == 1;
-
+    boolean NewIconStyle = Settings.Global.getInt(getContext().getContentResolver(),
+            Settings.Global.STATUSBAR_ICONS_STYLE, 0) == 1;
 
     public NotificationIconContainer(Context context, AttributeSet attrs) {
         super(context, attrs);
@@ -808,7 +810,7 @@ public class NotificationIconContainer extends AlphaOptimizedFrameLayout {
                     }
                 }
                 icon.setVisibleState(visibleState, animationsAllowed);
-                if (icon.getStatusBarIcon().pkg.contains("systemui"))
+                if (icon.getStatusBarIcon().pkg.contains("systemui") || !NewIconStyle)
                     icon.setIconColor(iconColor, needsCannedAnimation && animationsAllowed);
                 if (animate) {
                     animateTo(icon, animationProperties);
@@ -832,7 +834,7 @@ public class NotificationIconContainer extends AlphaOptimizedFrameLayout {
             super.initFrom(view);
             if (view instanceof StatusBarIconView) {
                 StatusBarIconView icon = (StatusBarIconView) view;
-                if (icon.getStatusBarIcon().pkg.contains("systemui"))
+                if (icon.getStatusBarIcon().pkg.contains("systemui") || !NewIconStyle)
                     iconColor = ((StatusBarIconView) view).getStaticDrawableColor();
                 else
                     return;